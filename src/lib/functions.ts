a9df37e25550d65ce933c02c32e68c41e023a4ea
Eyal Cohen <eyalcohen008@gmail.com>
2022-12-02 20:50:03 +0200

    Created utils file

---
+++ src/lib/functions.ts
@@ -0,0 +1,255 @@
+const createTvaiStroke = (): void => {
+  const comp = app.project.activeItem as CompItem;
+  const layer = comp.layers.addShape();
+
+  const contents = layer.property('ADBE Root Vectors Group') as PropertyGroup;
+  const shapeGrp = contents.addProperty('ADBE Vector Group');
+  shapeGrp.name = 'Shape 1';
+
+  const lineGrp = shapeGrp.property('ADBE Vectors Group') as PropertyGroup;
+  const pathGrp = lineGrp.addProperty('ADBE Vector Shape - Group');
+  const linePath = pathGrp.property('ADBE Vector Shape') as Property<any>;
+
+  const size = comp.width;
+  const myShape = new Shape();
+  myShape.vertices = [
+    [-(size / 2), 0],
+    [size / 2, 0]
+  ];
+  myShape.closed = false;
+
+  linePath.setValue(myShape);
+
+  const myStroke = lineGrp.addProperty(
+    'ADBE Vector Graphic - Stroke'
+  ) as Property<any>;
+  const strokeWidth = myStroke.property(
+    'ADBE Vector Stroke Width'
+  ) as Property<any>;
+  strokeWidth.setValue(10);
+
+  const dashesProp = myStroke.property(
+    'ADBE Vector Stroke Dashes'
+  ) as PropertyGroup;
+  const dashOne = dashesProp.addProperty(
+    'ADBE Vector Stroke Dash 1'
+  ) as Property<any>;
+  dashOne.setValue(25);
+  const dashOffset = dashesProp.addProperty(
+    'ADBE Vector Stroke Offset'
+  ) as Property<any>;
+  dashOffset.expression = 'time * effect("Speed")("Slider")';
+
+  const slider = layer.effect.addProperty(
+    'ADBE Slider Control'
+  ) as Property<any>;
+  slider.name = 'Speed';
+  const sliderVal = slider.property(
+    'ADBE Slider Control-0001'
+  ) as Property<any>;
+  sliderVal.setValue(50);
+
+  layer
+    .property('ADBE Root Vectors Group')
+    .property('ADBE Vector Group')
+    .property('ADBE Vectors Group')
+    .property('ADBE Vector Shape - Group').selected = true;
+};
+
+const scaleWithOvershoot = (): void => {
+  const comp = app.project.activeItem as CompItem;
+  const selectedLayers = comp.selectedLayers;
+  if (selectedLayers.length === 0) return;
+
+  selectedLayers.forEach(sl => {
+    const scaleProp = sl
+      .property('ADBE Transform Group')
+      .property('ADBE Scale') as Property<any>;
+    const origVal = scaleProp.value;
+
+    let beforeKeys: number = 0;
+    const numKeys = scaleProp.numKeys;
+    for (let i = 1; i <= numKeys; i++) {
+      const keyTime = scaleProp.keyTime(i);
+      if (keyTime < comp.time) beforeKeys++;
+    }
+
+    scaleProp.setValueAtTime(comp.time, [0, 0]);
+    scaleProp.setValueAtTime(comp.time + (1 / 24) * 10, [
+      origVal[0] + 5,
+      origVal[1] + 5
+    ]);
+    scaleProp.setValueAtTime(comp.time + (1 / 24) * 14, origVal);
+
+    var easeIn = new KeyframeEase(0.5, 66);
+    var easeOut = new KeyframeEase(0.75, 66);
+    scaleProp.setTemporalEaseAtKey(
+      beforeKeys + 1,
+      [easeIn, easeIn, easeIn],
+      [easeOut, easeOut, easeOut]
+    );
+    scaleProp.setTemporalEaseAtKey(
+      beforeKeys + 2,
+      [easeIn, easeIn, easeIn],
+      [easeOut, easeOut, easeOut]
+    );
+    scaleProp.setTemporalEaseAtKey(
+      beforeKeys + 3,
+      [easeIn, easeIn, easeIn],
+      [easeOut, easeOut, easeOut]
+    );
+  });
+};
+
+const importLogos = (): void => {
+  const idfItem = app.project.importFile(
+    new ImportOptions(
+      File('C:/Users/eyalc/DevProjects/mivtzai-utils/src/assets/IDF_Logo.png')
+    )
+  ) as AVItem;
+  const dotzItem = app.project.importFile(
+    new ImportOptions(
+      File('C:/Users/eyalc/DevProjects/mivtzai-utils/src/assets/Dotz_Logo.png')
+    )
+  ) as AVItem;
+
+  const comp = app.project.activeItem as CompItem;
+  const idfLayer = comp.layers.add(idfItem);
+
+  const padding = 200;
+
+  const idfScale = idfLayer
+    .property('ADBE Transform Group')
+    .property('ADBE Scale') as Property<any>;
+  idfScale.setValue([4, 4]);
+  const idfPos = idfLayer
+    .property('ADBE Transform Group')
+    .property('ADBE Position') as Property<any>;
+  idfPos.setValue([comp.width - padding, 0 + padding]);
+
+  const dotzLayer = comp.layers.add(dotzItem);
+  const dotzScale = dotzLayer
+    .property('ADBE Transform Group')
+    .property('ADBE Scale') as Property<any>;
+  dotzScale.setValue([67, 67]);
+  const dotzPos = dotzLayer
+    .property('ADBE Transform Group')
+    .property('ADBE Position') as Property<any>;
+  dotzPos.setValue([0 + padding, 0 + padding]);
+};
+
+const createIllustrationText = (): void => {
+  const comp = app.project.activeItem as CompItem;
+  const textLayer = comp.layers.addText();
+  const srcText = textLayer
+    .property('ADBE Text Properties')
+    .property('ADBE Text Document') as Property<any>;
+
+  srcText.setValue('אילוסטרציה');
+  const textDoc = srcText.value;
+  textDoc.font = 'NarkisBlockCondensedMF-Bold';
+  textDoc.fontSize = 100;
+  textDoc.applyFill = true;
+  textDoc.fillColor = [1, 1, 1];
+  textDoc.applyStroke = false;
+  textDoc.tracking = 0;
+  srcText.setValue(textDoc);
+
+  const boundingBox = textLayer.sourceRectAtTime(comp.time, false);
+  const layerPos = textLayer
+    .property('ADBE Transform Group')
+    .property('ADBE Position') as Property<any>;
+
+  const padding = 40;
+  layerPos.setValue([-boundingBox.left + padding, comp.height - padding]);
+};
+
+const formatLayerName = (): void => {
+  const comp = app.project.activeItem as CompItem;
+
+  const selLayers = comp.selectedLayers;
+  if (selLayers.length === 0) return;
+
+  for (let i = 0; i < selLayers.length; i++) {
+    const cur = selLayers[i];
+    const name = cur.name;
+
+    const capitalize = (str: string) => {
+      return str
+        .split(' ')
+        .map(function (word) {
+          return word.charAt(0).toUpperCase() + word.slice(1);
+        })
+        .join(' ');
+    };
+
+    cur.name = capitalize(name).replace(/ /g, '_');
+    if (cur instanceof AVLayer) {
+      cur.source.name = cur.name;
+    }
+  }
+};
+
+const textReverse = (): void => {
+  const comp = app.project.activeItem as CompItem;
+  const selLayers = comp.selectedLayers;
+
+  for (let i = 0; i < selLayers.length; i++) {
+    const curLayer = selLayers[i];
+    if (curLayer instanceof TextLayer) {
+      const srcTextProp = curLayer
+        .property('ADBE Text Properties')
+        .property('ADBE Text Document') as Property<any>;
+
+      const srcValue = srcTextProp.value.toString();
+      const srcValueReverse = srcValue.split('').reverse().join('');
+
+      srcTextProp.setValue(srcValueReverse.toString());
+    }
+  }
+};
+
+const createBg = (): void => {
+  const comp = app.project.activeItem as CompItem;
+  const layer = comp.layers.addShape();
+  layer.name = 'BG';
+  layer.label = 16;
+  // layer.locked = true;
+
+  const contents = layer.property('ADBE Root Vectors Group') as PropertyGroup;
+  const grp = contents.addProperty('ADBE Vector Group') as PropertyGroup;
+  grp.name = 'Rectangle 1';
+  const recGrp = grp.property('ADBE Vectors Group') as PropertyGroup;
+
+  const recShape = recGrp.addProperty('ADBE Vector Shape - Rect');
+  const recSize = recShape.property('ADBE Vector Rect Size') as Property<
+    [number, number]
+  >;
+  recSize.setValue([comp.width, comp.height]);
+
+  const gFill = recGrp.addProperty(
+    'ADBE Vector Graphic - G-Fill'
+  ) as Property<any>;
+
+  const gradType = gFill.property('ADBE Vector Grad Type') as Property<number>;
+  gradType.setValue(2);
+
+  const endPoint = gFill.property('ADBE Vector Grad End Pt') as Property<
+    [number, number]
+  >;
+  endPoint.setValue([comp.width / 2, 0]);
+
+  // const colors = gFill.property('ADBE Vector Grad Colors') as Property<any>;
+  // colors.setValue([
+  //   [255, 0, 0],
+  //   [0, 0, 255]
+  // ]);
+
+  const fx = layer.property('ADBE Effect Parade') as PropertyGroup;
+  const tint = fx.addProperty('ADBE Tint');
+  const tintBlack = tint.property('ADBE Tint-0001') as Property<any>;
+  const tintWhite = tint.property('ADBE Tint-0002') as Property<any>;
+
+  tintWhite.setValue([118 / 255, 15 / 255, 15 / 255]);
+  tintBlack.setValue([25 / 255, 0, 0]);
+};
