d7c71d3ff0d038d3851fe041ae0fc7304cfa6db8
Eyal Cohen <eyalcohen008@gmail.com>
2023-10-20 00:02:04 +0300

    New Text tab

--- src/lib/utils/extra.ts
+++ src/lib/utils/extra.ts
@@ -26,3 +26,22 @@ const getFontFromName = (name: CaspionFont) => {
         return 'JannaLT-Bold';
     }
 };
+
+const detectLanguage = (
+    input: string
+): 'Hebrew' | 'Arabic' | 'English' | null => {
+    // Regular expressions for Hebrew, English, and Arabic
+    const hebrewRegExp = /[\u0590-\u05FF]/;
+    const englishRegExp = /[A-Za-z]/;
+    const arabicRegExp = /[\u0600-\u06FF]/;
+
+    if (hebrewRegExp.test(input)) {
+        return 'Hebrew';
+    } else if (englishRegExp.test(input)) {
+        return 'English';
+    } else if (arabicRegExp.test(input)) {
+        return 'Arabic';
+    } else {
+        return null;
+    }
+};
